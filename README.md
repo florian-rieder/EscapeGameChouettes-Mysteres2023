bb2586b5666cfa8c566a8ec759d7d61c67817302
Sergenti <florian.rieder@unil.ch>
2023-05-21 19:19:35 +0200

    Atelier Mesures biométriques
    Very hardcody

--- README.md
+++ README.md
@@ -6,9 +6,13 @@ Un mini-jeu qui accompagne l'escape game organisée autour des chouettes pour le
 ### Terminé
 - Atelier "Nourriture"
 - Audio atelier "Nichoir"
+- Atelier "Mesures biométriques"
 
 ### A faire
 - Barre de vie
-- Atelier biométrie
 - Atelier nichoir
 - Styles
+
+
+## Assets
+Stanberry - par GrandChaos9000. Sous license Creative Commons (CC-BY-SA 3.0)
--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-// Shorthands: see https://stackoverflow.com/questions/13383886
+/* ------------------------------------------------------------------------- */
+/* ------------------------------ SHORTHANDS ------------------------------- */
+/* ------------------------------------------------------------------------- */
+
+
+// see https://stackoverflow.com/questions/13383886
 const query = document.querySelector.bind(document);
 const queryAll = document.querySelectorAll.bind(document);
 const fromId = document.getElementById.bind(document);
 const fromClass = document.getElementsByClassName.bind(document);
 const fromTag = document.getElementsByTagName.bind(document);
 
+function show(element) {
+    element.classList.remove("hidden");
+}
+
+function hide(element) {
+    element.classList.add("hidden");
+}
+
 
 /* ------------------------------------------------------------------------- */
 /* --------------------------------- SETUP --------------------------------- */
 /* ------------------------------------------------------------------------- */
+
 
 const sounds = {
     merle: new Howl({
@@ -58,8 +72,8 @@ backBtn.addEventListener("click", () => {
 });
 
 function displayHome() {
-    home.classList.remove("hidden");
-    workshops.classList.add("hidden");
+    show(home);
+    hide(workshops);
 }
 
 const foodWorkshopBtn = fromId("food-workshop-btn");
@@ -72,15 +86,15 @@ biometricWorkshopBtn.addEventListener("click", () => displayWorkshop("biometric-
 nestWorkshopBtn.addEventListener("click", () => displayWorkshop("nest-workshop"));
 
 function displayWorkshop(id) {
-    home.classList.add("hidden");
-    workshops.classList.remove("hidden");
+    hide(home);
+    show(workshops);
     // show the corresponding workshop and hide the others
     for (i = 0; i < workshopContainers.length; i++) {
         const container = workshopContainers[i];
         if (container.id == id) {
-            container.classList.remove("hidden");
+            show(container);
         } else {
-            container.classList.add("hidden");
+            hide(container);
         }
     }
 }
@@ -103,28 +117,31 @@ foodButtonA.addEventListener("click", foodChoiceA);
 foodButtonB.addEventListener("click", foodChoiceB);
 
 backBtnFoodChoicesA.addEventListener("click", () => {
-    foodResultA.classList.add("hidden");
-    foodChoices.classList.remove("hidden");
+    hide(foodResultA);
+    show(foodChoices);
 });
 
 backBtnFoodChoicesB.addEventListener("click", () => {
-    foodResultB.classList.add("hidden");
-    foodChoices.classList.remove("hidden");
+    hide(foodResultB);
+    show(foodChoices);
     displayHome();
 });
 
 function foodChoiceA() {
     /* Retour au choix de proie - Diminution de la ligne de vie */
-    foodResultA.classList.remove("hidden");
-    foodChoices.classList.add("hidden");
+    show(foodResultA);
+    hide(foodChoices);
+    foodButtonA.disabled = true;
+
 
     health -= 10;
 }
 
 function foodChoiceB() {
     /* Retour à la page principale - Augmentation de la ligne de vie */
-    foodResultB.classList.remove("hidden");
-    foodChoices.classList.add("hidden");
+    show(foodResultB);
+    hide(foodChoices);
+    foodWorkshopBtn.disabled = true;
 
     health += 20;
 }
@@ -134,6 +151,115 @@ function foodChoiceB() {
 /* -------------------------- BIOMETRIC WORKSHOP --------------------------- */
 /* ------------------------------------------------------------------------- */
 
+// I admit, not my finest work...
+// Step 1 - Etat général
+const bioStep1 = fromId("bio-step-1");
+const bioChoices1 = fromId("bio-choices-1");
+const bioChoice1A = fromId("bio-choice-1A");
+const bioChoice1B = fromId("bio-choice-1B");
+const bioResult1A = fromId("bio-result-1A");
+const bioResult1B = fromId("bio-result-1B");
+const bioBack1A  = fromId("back-bio-choice-1A");
+const bioBack1B  = fromId("back-bio-choice-1B");
+
+// Step 2 - Condition physique
+const bioStep2 = fromId("bio-step-2");
+const bioChoices2 = fromId("bio-choices-2");
+const bioChoice2A = fromId("bio-choice-2A");
+const bioChoice2B = fromId("bio-choice-2B");
+const bioResult2A = fromId("bio-result-2A");
+const bioResult2B = fromId("bio-result-2B");
+const bioBack2A  = fromId("back-bio-choice-2A");
+const bioBack2B  = fromId("back-bio-choice-2B");
+
+// Step 3 - Plumes
+const bioStep3 = fromId("bio-step-3");
+const bioChoices3 = fromId("bio-choices-3");
+const bioChoice3A = fromId("bio-choice-3A");
+const bioChoice3B = fromId("bio-choice-3B");
+const bioResult3A = fromId("bio-result-3A");
+const bioResult3B = fromId("bio-result-3B");
+const bioBack3A  = fromId("back-bio-choice-3A");
+const bioBack3B  = fromId("back-bio-choice-3B");
+
+
+// Step 1
+bioChoice1A.addEventListener("click", () => {
+    hide(bioChoices1);
+    show(bioResult1A);
+});
+
+bioChoice1B.addEventListener("click", () => {
+    hide(bioChoices1);
+    show(bioResult1B);
+});
+
+bioBack1A.addEventListener("click", () => {
+    /* Retour à la page précédente "Etat général" pour permettre de choisir "Tout est ok" */
+    show(bioChoices1);
+    hide(bioResult1A);
+    bioChoice1A.disabled = true;
+});
+
+bioBack1B.addEventListener("click", () => {
+    /* Avancer à la page - Condition physique */
+    hide(bioStep1);
+    show(bioStep2);
+});
+
+
+// Step 2
+bioChoice2A.addEventListener("click", () => {
+    hide(bioChoices2);
+    show(bioResult2A);
+});
+
+bioChoice2B.addEventListener("click", () => {
+    hide(bioChoices2);
+    show(bioResult2B);
+});
+
+bioBack2A.addEventListener("click", () => {
+    /* Retour à la page précédente "Condition physique" pour permettre de choisir "un des 2 mâles est plus léger" */
+    show(bioChoices2);
+    hide(bioResult2A);
+    bioChoice2A.disabled = true;
+
+});
+
+bioBack2B.addEventListener("click", () => {
+    /* Avancer à la page - Etat des plumes */
+    hide(bioStep2);
+    show(bioStep3);
+});
+
+
+// Step 3
+bioChoice3A.addEventListener("click", () => {
+    hide(bioChoices3);
+    show(bioResult3A);
+});
+
+bioChoice3B.addEventListener("click", () => {
+    hide(bioChoices3);
+    show(bioResult3B);
+});
+
+bioBack3A.addEventListener("click", () => {
+    /* Retour à la page précédente "Etat des plumes" pour permettre de choisir "un des 2 mâles a perdu la première plume" */
+    show(bioChoices3);
+    hide(bioResult3A);
+    bioChoice3A.disabled = true;
+
+});
+
+bioBack3B.addEventListener("click", () => {
+    /* Augmentation de la ligne de vie - Retour à la page principale */
+    hide(bioResult3B);
+    biometricWorkshopBtn.disabled = true;
+    displayHome();
+    health += 30
+});
 
 /* ------------------------------------------------------------------------- */
 /* ----------------------------- NEST WORKSHOP ----------------------------- */
@@ -142,11 +268,10 @@ function foodChoiceB() {
 
 fromId('nest-btn-merle').addEventListener('click', () => toggleSound(sounds.merle));
 fromId('nest-btn-mesange').addEventListener('click', () => toggleSound(sounds.mesange));
-fromId('nest-btn-effraie').addEventListener('click', () => toggleSound(sounds.effraie));
-//fromId('nest-btn-effraie2').addEventListener('click', () => toggleSound(sounds.effraie2));
 fromId('nest-btn-loriot').addEventListener('click', () => toggleSound(sounds.loriot));
 fromId('nest-btn-hulotte').addEventListener('click', () => toggleSound(sounds.hulotte));
-
+fromId('nest-btn-effraie').addEventListener('click', () => toggleSound(sounds.effraie));
+//fromId('nest-btn-effraie2').addEventListener('click', () => toggleSound(sounds.effraie2));
 
 function toggleSound(sound) {
     if (sound.playing()) {
